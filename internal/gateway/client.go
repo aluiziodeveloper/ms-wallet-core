8e16bfb32ce0b760786908f8dd041c85ae97cb1a
Jorge Aluizio <aluiziodeveloper@gmail.com>
2023-04-08 12:31:19 -0300

    Creating usecase create_client

---
+++ internal/gateway/client.go
@@ -0,0 +1,10 @@
+package gateway
+
+import (
+	"github.com/aluiziodeveloper/ms-wallet-core/internal/entity"
+)
+
+type ClientGateway interface {
+	Get(id string) (*entity.Client, error)
+	Save(client *entity.Client) error
+}
